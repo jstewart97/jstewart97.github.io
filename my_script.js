ca44c41b468177a5bcac94ffb6c47631c27b6568
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-07-21 12:38:31 -0500

    first files

---
+++ my_script.js
@@ -0,0 +1,16 @@
+function goToLunch(student) {
+  //Instruction go here.
+  console.log(student + ", get up from chair");
+  console.log(student + ", walk to isle");
+  console.log(student + ", walk to door and leave");
+  return student.toUpperCase();
+
+}
+
+function add(firstNumber, secondNumber) {
+  return firstNumber  + secondNumber;
+}
+
+goToLunch("Shaun");
+goToLunch("Stew");
+goToLunch("Josh");
